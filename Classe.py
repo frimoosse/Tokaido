5de51b03dee48fd274b730877882a0c86d635857
Frimoosse <sam76420@gmail.com>
2023-04-19 17:32:24 +0200

    update interface, création class station

--- Classe.py
+++ Classe.py
@@ -1,6 +1,9 @@
 # classe
 import Card_effect_rencontre as cer
 import fonction as f
+import random as r
+import csv
+import Main
 
 class joueur ():
     def __init__ (self, pseudo, colour):
@@ -45,7 +48,7 @@ class station():
         self.chara = []     # nombre de personne sur la case <= self.place
 
 class temple (station):
-    def effect (player):
+    def effect (player, repas,items,nb_joueur):
         offrande = int(input("nombre de piece a offrir"))
         while offrande > player.yen:
             offrande = int(input("nombre de piece a offrir"))
@@ -57,7 +60,7 @@ class temple (station):
         player.point += offrande
 
 class rencontre (station):
-    def effect (player) :
+    def effect (player, repas,items,nb_joueur) :
         if player.character == "Yoshiyasu":
             p=[]
             pioche = f.pioche(2,station,p)[0]
@@ -73,3 +76,163 @@ class rencontre (station):
         if player.character == "Umegae":
             player.yen += 1
             player.point += 1
+
+class source (station):
+    def effect (player, repas,items,nb_joueur):
+        pt = r.randint(2,3)
+        player.point += pt
+        if player.character == "Mitsukuni":
+            player.point += 1
+
+class riziere (station):
+    def effect (player, repas,items,nb_joueur):
+        rencontre = 'Annaibito Rizière'
+        cer.effet(rencontre,player)
+
+class montagne (station):
+    def effect (player, repas,items,nb_joueur):
+        rencontre = 'Annaibito Montagne'
+        cer.effet( rencontre ,player)
+
+class mer (station):
+    def effect (player, repas,items,nb_joueur):
+        rencontre = 'Annaibito Mer'
+        cer.effet(rencontre,player)
+
+class ferme (station):
+    def effect (player, repas,items,nb_joueur):
+        player.yen += 3
+
+class relais (station):
+    def effect (player, repas,items,nb_joueur):
+        if player.character == "Chuubei":
+            p=[]
+            pioche = f.pioche(1,"rencontre",p)[0]
+            print(pioche)
+            cer.effet(pioche[0],player)
+        if player.character == "Hiroshige":
+            ask = int(input("which panorama choice ? "))
+            if ask == 1:
+                rencontre = 'Annaibito Mer'
+                cer.effet(rencontre,player)
+            elif ask == 2:
+                rencontre = 'Annaibito Montagne'
+                cer.effet( rencontre ,player)
+            elif ask == 3:
+                rencontre = 'Annaibito Rizière'
+                cer.effet(rencontre,player)
+
+
+        if Main.gastronomie and relais == 0:                                            # 
+            repas.actuel,poi = (f.pioche(nb_joueur,"repas",repas.precedent))            # Si Gastronomie ###############
+            for a in range (len(repas.actuel)):                                         #
+                repas.precedent.append(poi[a])  
+
+        elif relais == 0:
+            repas.actuel,poi = (f.pioche(nb_joueur+1,"repas",repas.precedent))
+            for a in range (len(repas.actuel)):
+                repas.precedent.append(poi[a])
+
+        
+        
+        choix = 0
+        while choix != -2:
+            print(repas.actuel)
+            
+            choix = int(input("repas choisi"))
+
+            if choix == -1:
+                return
+
+            with open('repas.csv') as repas2:                      
+                csv_reader = csv.reader(repas2, delimiter = ',')
+                nom_repas = repas.actuel[choix]
+                for row in csv_reader:
+                    if str(nom_repas) == row[0]:
+                        prix = row[1]
+
+            if player.character == "Kinko":
+                if (int(prix)-1) <= player.yen:
+                    del(repas.actuel[choix])
+                    player.point += 6
+                    player.yen -= (int(prix)-1)
+                    choix = -2
+                    
+            else:
+                if int(prix) < player.yen:
+                    del(repas.actuel[choix])
+                    player.point += 6
+                    player.yen -= int(prix)
+                    choix = -2
+
+            
+class echoppe (station):
+    def effect (player, repas,items,nb_joueur):
+        achat = []
+        pioche_memory =[]
+        pioche,memoire = f.pioche(3,station,items.allcard)
+        for it in pioche:
+            pioche_memory.append(it)
+        items.allcard.append(memoire)
+        print(pioche)
+        choix = int(input("quel item? " ))
+        if choix != -1:
+            achat.append(pioche[choix])
+            del(pioche[choix])
+            print(pioche)
+            choix = int(input("autre item ? "))
+            if choix != -1:
+                achat.append(pioche[choix])
+                del(pioche[choix])
+                print(pioche)
+                choix = int(input("un dernire item ? "))
+                if choix != -1:
+                    achat.append(pioche[choix])
+        prix = 0
+        with open('List_souvenirs.csv') as souvenir:                      
+            csv_reader = csv.reader(souvenir, delimiter = ',')
+            for row in csv_reader:
+                for count in achat:
+                    nom_souvenir = str(count)
+                    if str(nom_souvenir) == row[1]:
+                        prix += int(row[2])
+        if prix > player.yen:
+            achat = -2
+            pioche = []
+            for it in pioche_memory:
+                pioche.append(it)
+        while achat == -2:
+            achat = []
+            print("voux n'avez pas assez d'argent")
+            print(pioche)
+            choix = int(input("quel item? " ))
+            if choix != -1:
+                achat.append(pioche[choix])
+                del(pioche[choix])
+                print(pioche)
+                choix = int(input("autre item ? "))
+                if choix != -1:
+                    achat.append(pioche[choix])
+                    del(pioche[choix])
+                    print(pioche)
+                    choix = int(input("un dernire item ? "))
+                    if choix != -1:
+                        achat.append(pioche[choix])
+            
+            prix = 0
+            with open('List_souvenirs.csv') as souvenir:                      
+                csv_reader = csv.reader(souvenir, delimiter = ',')
+                for row in csv_reader:
+                    for count in achat:
+                        nom_souvenir = str(count)
+                        if str(nom_souvenir) == row[1]:
+                            prix += int(row[2])
+            if prix > player.yen:
+                achat = -2
+                pioche = []
+                for it in pioche_memory:
+                    pioche.append(it)
+
+        
+        player.yen -= prix
+        player.inventaire.append(achat)
--- interface.py
+++ interface.py
@@ -9,8 +9,7 @@ clock = pygame.time.Clock()
 
 # display
 screen = pygame.display.set_mode((1920,1080))
-board = pygame.image.load("IMAGE/piste_2.jpeg")
-board = pygame.transform.scale(board,[1920,1080])
+board = pygame.image.load("IMAGE/piste.jpg")
 board_rect = board.get_rect(topleft = [0,0])
 
 moving = False
@@ -31,8 +30,13 @@ while True:
 
     pygame.display.flip()
     screen.fill((0,0,0))
+    board_rect.top = 0
+    if board_rect.left > 0:
+       board_rect.left = 0
+    elif board_rect.right < 1920:
+       board_rect.right = 1920
     screen.blit(board,board_rect)
-    clock.tick(240)
+    clock.tick(244)
 
 
 pygame.quit()
